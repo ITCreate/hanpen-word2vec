d35793f172f3b4187603ed92ac466c7ab6e1c303
ReiOogami <b4049@oic.jp>
2020-08-01 17:46:03 +0900

    修正

--- jsonProcessor/index.php
+++ jsonProcessor/index.php
@@ -2,17 +2,11 @@
 
 $json = json_decode(file_get_contents('../word.json'), true);
 
-$xList = [];
-$yList = [];
 $distanceList = [];
 $returnList = [];
 foreach ($json as $key => $item) {
-    $xList[] = $item['x'];
-    $yList[] = $item['y'];
     $json[$key]['distance'] = $distanceList[] = sqrt(pow($item['x'] - 0, 2) + pow($item['y'] - 0, 2));
 }
-
-var_dump(max($distanceList), min($distanceList));
 
 foreach ($json as $key => $item) {
     if($item['distance'] < 10) {
